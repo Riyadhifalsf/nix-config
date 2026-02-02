0352576a0f574eab98031747d9231bffa6847231
installer <installer@gmail.com>
2026-02-02 16:39:08 +0700

    updated zsh

--- modules/programs/zsh/zsh.nix
+++ modules/programs/zsh/zsh.nix
@@ -1,28 +1,40 @@
 { config, pkgs, ... }:
-
 {
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
+    programs.zsh = {
+        enable = true;
 
-    syntaxHighlighting.enable = true;
-    autosuggestion.enable = true;
+        enableCompletion = true;
+        syntaxHighlighting.enable = true;
+        
+        # history = {
+        #     enable = true;
+        #     path = "$HOME/.zsh_history";
+        #     size = 10000;
+        #     save = 10000;
+        #     share = true;
+        #     ignoreDups = true;
+        #     ignoreSpace = true;
+        # };
 
-    shellAliases = {
-      ll = "ls -lah";
-      gs = "git status";
+        shellAliases = {
+            ll = "ls -lah";
+            gs = "git status";
+        };
+
+        initExtra = ''
+            # load powerlevel10k
+            source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+
+            # load declarative p10k config
+            source ${./p10k.zsh}
+        '';
+
     };
 
-    initExtra = ''
-      # load powerlevel10k theme
-      source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-
-      # load declarative p10k config
-      [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
-    '';
-  };
-
-  # COPY p10k config ke HOME (INI KUNCI UTAMANYA)
-  home.file.".p10k.zsh".source = ./p10k.zsh;
+    programs.zsh.autosuggestion.enable = true;
+    # programs.starship = {
+    #     enable = true;
+    #     enableZshIntegration = true;
+    # };  
 
 }
