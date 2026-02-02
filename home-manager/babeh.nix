d66769abd6a7f8225c4d6a3b696bccbf0e9a2fa2
installer <installer@gmail.com>
2026-02-03 00:30:09 +0700

    updates desktop application to dock path

--- home-manager/babeh.nix
+++ home-manager/babeh.nix
@@ -2,6 +2,7 @@
 
 {
     imports = [
+        ../modules/programs/gpg-agent.nix
         ../modules/programs/zsh/zsh.nix
         ./desktop-kde-applications.nix
     ];
@@ -34,29 +35,8 @@
         zsh-syntax-highlighting
 
         tree
-
         pkgs.zsh-powerlevel10k
 
     ];
-
-
-    services.gpg-agent.enable = true;
-
-    home.file.".config/plasmashellrc".source =
-    ../modules/desktop/kde-plasma/config/plasmashellrc;
-
-    home.file.".config/plasma-org.kde.plasma.desktop-appletsrc".source =
-    ../modules/desktop/kde-plasma/config/plasma-org.kde.plasma.desktop-appletsrc;
-
-    home.file.".config/kdeglobals".source =
-    ../modules/desktop/kde-plasma/config/kdeglobals;
-
-    # icon launcher
-    home.file.".local/share/icons/icon-launcher.png".source =
-    ../modules/desktop/kde-plasma/config/assets/icon-launcher.png;
-    
-    # wallpaper
-    home.file.".local/share/wallpapers/wallpaper.jpg".source =
-    ../modules/desktop/kde-plasma/config/assets/wallpaper.jpg;
 
 }
