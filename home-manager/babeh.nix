65802951f8a8bd311a61efaa994bdc68a01ff947
installer <installer@gmail.com>
2026-02-03 00:44:32 +0700

    update folder and config path home manager for kde plasma home

--- home-manager/babeh.nix
+++ home-manager/babeh.nix
@@ -4,7 +4,8 @@
     imports = [
         ../modules/programs/gpg-agent.nix
         ../modules/programs/zsh/zsh.nix
-        ./desktop-kde-applications.nix
+        ./config/kde/plasma-home.nix
+        ./config/kde/desktop-home.nix
     ];
 
     home.stateVersion = "24.11";
@@ -22,20 +23,18 @@
         telegram-desktop
         bottles
         wpsoffice
-        zsh
         neovim
-        pkgs.google-chrome
-        
         arduino-ide
         planify
         codeblocksFull
         distrobox
+        tree
 
+        # Initial setup zsh packages
+        zsh
         zsh-autosuggestions
         zsh-syntax-highlighting
-
-        tree
-        pkgs.zsh-powerlevel10k
+        pkgs.zsh-powerlevel10kfff
 
     ];
 
---
+++ home-manager/config/kde/plasma-home.nix
@@ -0,0 +1,18 @@
+{
+    home.file.".config/plasmashellrc".source =
+    ./config/kde/plasmashellrc;
+
+    home.file.".config/plasma-org.kde.plasma.desktop-appletsrc".source =
+    ./config/kde/plasma-org.kde.plasma.desktop-appletsrc;
+
+    home.file.".config/kdeglobals".source =
+    ./config/desktop/kde/kdeglobals;
+
+    # icon launcher
+    home.file.".local/share/icons/icon-launcher.png".source =
+    ./config/desktop/kde/assets/icon-launcher.png;
+    
+    # wallpaper
+    home.file.".local/share/wallpapers/wallpaper.jpg".source =
+    ./config/desktop/kde/assets/wallpaper.jpg;
+}
